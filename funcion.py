6c8e5804e039ddbac65c020f82351513075616fb
lmiel <lucia.mielgo@gmail.com>
2022-12-15 23:49:00 +0100

    prueba inicial

--- funcion.py
+++ funcion.py
@@ -5,15 +5,15 @@ import re
 import sys
 
 def gameOfStones(n):
-
-
+    print("Partida con {} piedras".format(n))
+    return str(n)
 if __name__ == '__main__': 
-    fptr = open(os.environ['OUTPUT_PATH'], 'w') 
+    fptr = open('resultados.txt', 'w') #abre el archivo resultados.txt en modo write para guardar los resultados
 
-    t = int(input().strip()) 
+    t = int(input().strip()) #numero de partidas que va a haber
 
-    for t_itr in range(t): 
-        n = int(input().strip())
+    for t_itr in range(t): #para cada partida del numero t
+        n = int(input().strip()) #n va a valer lo que escribas en la terminal, hay q escribir t nº de n
         result = gameOfStones(n) 
 
         fptr.write(result + '\n') 
