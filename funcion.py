553c8d7081d9982617edbabd333f4ccd4ee1ae0f
jone <jvegadel@myuax.com>
2022-12-16 09:02:31 +0100

    cambio

--- funcion.py
+++ funcion.py
@@ -1,21 +1,25 @@
-import math 
-import os 
-import random 
-import re 
+import math
+import os
+import random
+import re
 import sys
 
 def gameOfStones(n):
     print("Partida con {} piedras".format(n))
+    if n%7==0 or n%7==1:
+        return "Second"
+    else:
+        return "First"
     return str(n)
-if __name__ == '__main__': 
+if __name__ == '__main__':
     fptr = open('resultados.txt', 'w') #abre el archivo resultados.txt en modo write para guardar los resultados
 
     t = int(input().strip()) #numero de partidas que va a haber
 
     for t_itr in range(t): #para cada partida del numero t
         n = int(input().strip()) #n va a valer lo que escribas en la terminal, hay q escribir t nº de n
-        result = gameOfStones(n) 
+        result = gameOfStones(n)
 
-        fptr.write(result + '\n') 
-        
+        fptr.write(result + '\n')
+
     fptr.close()
