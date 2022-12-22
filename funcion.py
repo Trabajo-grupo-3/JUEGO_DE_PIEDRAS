3d2dc3cb0ca82528667a45c2c5c11cceff4c99b3
lmiel <lucia.mielgo@gmail.com>
2022-12-22 13:25:13 +0100

    algoritmo recursivo

--- funcion.py
+++ funcion.py
@@ -4,9 +4,25 @@ import random
 import re
 import sys
 
-def gameOfStones(n):
-    print("Partida con {} piedras".format(n))
-    return str(n)
+posiciones = {}
+
+def gameOfStones(n, movimientopos):
+    if n==1 or n==0:
+        return False
+    elif n==2 or n==3 or n==4 or n==5 or n==6:
+        return True
+    for movimiento in movimientopos:
+        if n-movimiento in posiciones.keys():
+            return posiciones.get(n-movimiento)
+        else:
+            variable = gameOfStones(n-movimiento, movimientopos)
+            posiciones.update({n-movimiento:variable})
+        if variable == False:
+            return True
+    return False
+
+
+
 if __name__ == '__main__':
     fptr = open('resultados.txt', 'w') #abre el archivo resultados.txt en modo write para guardar los resultados
 
@@ -14,22 +30,14 @@ if __name__ == '__main__':
 
     for t_itr in range(t): #para cada partida del numero t
         n = int(input("¿Con cuantas piedras quieres jugar en esta partida?: ").strip()) #n va a valer lo que escribas en la terminal, hay q escribir t nº de n
-        if n == 1:
-            print("Pierdes")
-        elif n == 2:
-            print("P1 elimina piedras y gana")
-        elif n == 3:
-            print("P1 elimina dos piedras dejando una y gana")
-        elif n == 4:
-            print("P1 elimina tres piedras dejando una y gana")
-        elif n == 5:
-            print("P1 elimina todas las piedras ganando")
-        elif n == 6:
-            print("P1 elimina cinco piedras dejando una y gana")
-        elif n == 7:
-            print("P1 elimina dos piedras, dejando cinco que coge P2 y gana P2", "P1 elimina cinco piedras dejando dos que coge P2 y gana P2", "P1 elimina 3 piedras dejando cuatro que P2 coge tres y gana P2")
-        result = gameOfStones(n)
-
-        fptr.write(result + '\n')
+        print("Partida con {} piedras".format(n))
+        result = gameOfStones(n, [2,3,5])
+        if result == True:
+            print("Gana el jugador 1")
+            fptr.write("Gana el jugador 1" + '\n')
+        else:
+            print("Gana el jugador 2")
+            fptr.write("Gana el jugador 2" + '\n')
+        
 
     fptr.close()
